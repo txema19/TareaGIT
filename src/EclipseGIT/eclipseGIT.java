ba0851d620ab8c4a68284b34ad5b69d7376bd806
TxemaValdes <josemaria.s317461@cesurformacion.com>
2023-03-08 19:05:32 +0100

    Segundo commit

--- src/EclipseGIT/eclipseGIT.java
+++ src/EclipseGIT/eclipseGIT.java
@@ -4,5 +4,6 @@ public class eclipseGIT {
 
 	public static void main (String[] args) {
 		System.out.println("Primer comando de eclipse GIT");
+		System.out.println("Segundo comando de eclipse GIT");
 	}
 }
