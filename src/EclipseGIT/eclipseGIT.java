96316cce028de69cfbbff78a45e02ed7b84f9c19
TxemaValdes <josemaria.s317461@cesurformacion.com>
2023-03-08 19:26:01 +0100

    Primer commit desde rama 2

--- src/EclipseGIT/eclipseGIT.java
+++ src/EclipseGIT/eclipseGIT.java
@@ -5,5 +5,6 @@ public class eclipseGIT {
 	public static void main (String[] args) {
 		System.out.println("Primer comando de eclipse GIT");
 		System.out.println("Segundo comando de eclipse GIT");
+		System.out.println("Primer comando desde Rama2");
 	}
 }
